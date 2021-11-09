d336a15fb107ae864273a1cb5ebda4ce9283ffb8
Maksim Tochilkin <mtoch777@gmail.com>
2021-11-08 23:33:24 -0500

    Initial Commit

---
+++ Networking_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Networking_Demo
+//
+//  Created by Maksim Tochilkin on 11/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Networking_Demo/Networking_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_DemoApp.swift
+//  Networking_Demo
+//
+//  Created by Maksim Tochilkin on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Networking_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
